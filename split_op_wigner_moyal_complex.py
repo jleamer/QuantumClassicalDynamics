64fba1161cc4c51fa05c496912b90abdfe586dc2
dbondar <kdfstudio@gmail.com>
2016-03-22 21:50:31 -0400

    Real valued Moyal-Wigner propagator added.

--- split_op_wigner_moyal_complex.py
+++ split_op_wigner_moyal_complex.py
@@ -1,8 +1,5 @@
 import numpy as np
-
-# numpy.fft has better implementation of real fourier transform
-# necessary for real split operator propagator
-from numpy import fft
+from scipy import fftpack # Tools for fourier transform
 
 
 class SplitOpWignerMoyal:
@@ -11,7 +8,11 @@ class SplitOpWignerMoyal:
     with the time-dependent Hamiltonian H = K(p, t) + V(x, t).
     (K and V may not depend on time.)
 
-    This implementation stores the Wigner function as a 2D real array.
+    This implementation stores the Wigner function as a 2D complex array.
+    Whereas, the Wigner function is theoretically real. Thus, the imaginary part of the implementation
+    should be due to numerical errors.
+
+    The split operator preserving Wigner function's reality is implemented in split_op_wigner_moyal.py
     """
     def __init__(self, **kwargs):
         """
@@ -36,16 +37,10 @@ class SplitOpWignerMoyal:
         except AttributeError:
             raise AttributeError("Coordinate grid size (X_gridDIM) was not specified")
 
-        assert 2**int(np.log2(self.X_gridDIM)) == self.X_gridDIM, \
-            "Coordinate grid size (X_gridDIM) should be power of 2"
-
         try:
             self.P_gridDIM
         except AttributeError:
             raise AttributeError("Momentum grid size (P_gridDIM) was not specified")
-
-        assert 2**int(np.log2(self.P_gridDIM)) == self.P_gridDIM, \
-            "Momentum grid size (P_gridDIM) should be power of 2"
 
         try:
             self.X_amplitude
@@ -87,10 +82,7 @@ class SplitOpWignerMoyal:
         self.X = self.X[np.newaxis, :]
 
         # Lambda grid (variable conjugate to the coordinate)
-        self.Lambda = fft.fftfreq(self.X_gridDIM, self.dX/(2*np.pi))
-        # take only first half, as required by the real fft
-        self.Lambda = self.Lambda[:(1 + self.X_gridDIM/2)]
-        #
+        self.Lambda = fftpack.fftfreq(self.X_gridDIM, self.dX/(2*np.pi))
         self.Lambda = self.Lambda[np.newaxis, :]
 
         # momentum grid
@@ -98,9 +90,7 @@ class SplitOpWignerMoyal:
         self.P = self.P[:, np.newaxis]
 
         # Theta grid (variable conjugate to the momentum)
-        self.Theta = fft.fftfreq(self.P_gridDIM, self.dP/(2*np.pi))
-        # take only first half, as required by the real fft
-        self.Theta = self.Theta[:(1 + self.P_gridDIM/2)]
+        self.Theta = fftpack.fftfreq(self.P_gridDIM, self.dP/(2*np.pi))
         self.Theta = self.Theta[:, np.newaxis]
 
         try:
@@ -135,18 +125,18 @@ class SplitOpWignerMoyal:
         for _ in xrange(time_steps):
 
             # p x -> theta x
-            self.wignerfunction = fft.rfft(self.wignerfunction, axis=0)
+            self.wignerfunction = fftpack.fft(self.wignerfunction, axis=0, overwrite_x=True)
             self.wignerfunction *= self.get_expV(self.t)
 
             # theta x  ->  p x
-            self.wignerfunction = fft.irfft(self.wignerfunction, axis=0)
+            self.wignerfunction = fftpack.ifft(self.wignerfunction, axis=0, overwrite_x=True)
 
             # p x  ->  p lambda
-            self.wignerfunction = fft.rfft(self.wignerfunction, axis=1)
+            self.wignerfunction = fftpack.fft(self.wignerfunction, axis=1, overwrite_x=True)
             self.wignerfunction *= self.get_expK(self.t)
 
             # p lambda  ->  p x
-            self.wignerfunction = fft.irfft(self.wignerfunction, axis=1)
+            self.wignerfunction = fftpack.ifft(self.wignerfunction, axis=1, overwrite_x=True)
 
             # normalization
             self.wignerfunction /= self.wignerfunction.sum() * dXdP
@@ -190,10 +180,8 @@ class SplitOpWignerMoyal:
         assert new_wigner_func.shape == (self.P.size, self.X.size), \
             "The grid sizes does not match with the Wigner function"
 
-        assert new_wigner_func.dtype == np.float, "Supplied Wigner function must be real"
-
         # make sure the Wigner function is stored as a complex array
-        self.wignerfunction = new_wigner_func.copy()
+        self.wignerfunction = new_wigner_func + 0j
 
         # normalize
         self.wignerfunction /= self.wignerfunction.sum() * self.dX*self.dP
@@ -274,20 +262,20 @@ if __name__ == '__main__':
                 t=0,
                 dt=0.005,
                 X_gridDIM=256,
-                X_amplitude=8.,
+                X_amplitude=7.,
                 P_gridDIM=256,
-                P_amplitude=8.,
+                P_amplitude=5.,
                 # kinetic energy part of the hamiltonian
                 K=lambda p: 0.5*p**2,
                 # potential energy part of the hamiltonian
-                V=lambda x: 0.5*9*x**2,
+                V=lambda x: 0.5*(2)**2*x**2,
                 # the force, i.e., F = -diff(V, x)
                 # Note: this functions are not used for propagation that are used below
-                F=lambda x: -9*x
+                F=lambda x: -2**2 * x
             )
 
             # parameter controling the width of the wigner function
-            sigma = np.random.uniform(1., 3.)
+            sigma = np.random.uniform(0.5, 3.)
 
             # set randomised initial condition
             self.quant_sys.set_wignerfunction(
@@ -334,7 +322,7 @@ if __name__ == '__main__':
 
             for _ in xrange(20):
                 # get the Wigner function in the coordinate representation
-                W = self.quant_sys.propagate()
+                W = self.quant_sys.propagate().real
 
                 self.average_X.append(np.sum(W * self.quant_sys.X) * dXdP)
                 self.average_P.append(np.sum(W * self.quant_sys.P) * dXdP)
@@ -388,3 +376,5 @@ if __name__ == '__main__':
     plt.xlabel('time $t$ (a.u.)')
 
     plt.show()
+
+
